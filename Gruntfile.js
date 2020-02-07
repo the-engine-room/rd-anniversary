e06b0146254a49ff755716def228c8e59606325d
Yavin <tech@alanzard.com>
2020-02-07 10:45:27 +0100

    Structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,111 +30,31 @@ module.exports = function(grunt) {
           ],
         },
         files: {
-          'css/rdf_main.css': 'src/less/main.less'
+          'css/rda_main.css': 'src/less/main.less'
         }
       }
     },
 
-    /*
+
     copy: {
 			build: {
 				files: [
 					{
-						src: 'public/src/components/jquery/dist/jquery.min.js',
-						dest: 'public/js/vendor/jquery.min.js'
+						src: 'src/components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js',
+						dest: 'js/jquery.mCustomScrollbar.concat.min.js'
+					},
+          {
+						src: 'src/components/jquery/dist/jquery.min.js',
+						dest: 'js/jquery.min.js'
 					},
 					{
-						src: 'public/src/components/bootstrap/dist/js/bootstrap.min.js',
-						dest: 'public/js/vendor/bootstrap.min.js'
+						src: 'src/components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css',
+						dest: 'css/jquery.mCustomScrollbar.min.css'
 					},
-          {
-						src: 'public/src/components/bootstrap/dist/css/bootstrap.min.css',
-						dest: 'public/css/vendor/bootstrap.min.css'
-					},
-					{
-						src: 'public/src/components/jquery.bootgrid/dist/jquery.bootgrid.min.js',
-						dest: 'public/js/vendor/jquery.bootgrid.min.js'
-					},
-					{
-						src: 'public/src/components/jquery.bootgrid/dist/jquery.bootgrid.far.min.js',
-						dest: 'public/js/vendor/jquery.bootgrid.fa.min.js'
-					},
-					{
-						src: 'public/src/components/jquery.bootgrid/dist/jquery.bootgrid.min.css',
-						dest: 'public/css/vendor/jquery.bootgrid.min.css'
-					},
-					{
-						src: 'public/src/components/bootstrap-select/dist/js/bootstrap-select.min.js',
-						dest: 'public/js/vendor/bootstrap-select.min.js'
-					},
-					{
-						src: 'public/src/components/bootstrap-select/dist/css/bootstrap-select.min.css',
-						dest: 'public/css/vendor/bootstrap-select.min.css'
-					},
-					{
-						src: 'public/src/components/jasny/dist/js/jasny-bootstrap.min.js',
-						dest: 'public/js/vendor/jasny-bootstrap.min.js'
-					},
-          {
-						src: 'public/src/components/moment/min/moment-with-locales.min.js',
-						dest: 'public/js/vendor/moment-with-locales.min.js'
-					},
-          {
-						src: 'public/src/components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
-						dest: 'public/js/vendor/bootstrap-datetimepicker.min.js'
-					},
-          {
-						src: 'public/src/components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
-						dest: 'public/css/vendor/bootstrap-datetimepicker.min.css'
-					},
-					{
-						src: 'public/src/components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css',
-						dest: 'public/css/vendor/jasny-bootstrap.min.css'
-					},
-          {
-						src: 'public/src/components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js',
-						dest: 'public/js/vendor/jasny-bootstrap.min.js'
-					},
-          {
-						src: 'public/src/components/css-toggle-switch/dist/toggle-switch.css',
-						dest: 'public/css/vendor/toggle-switch.css'
-					},
-          {
-						src: 'public/src/components/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css',
-						dest: 'public/css/vendor/awesome-bootstrap-checkbox.css'
-					},
-          {
-						src: 'public/src/components/bootstrap-validator/dist/validator.min.js',
-						dest: 'public/js/vendor/validator.min.js'
-					},
-          {
-						src: 'public/src/components/summernote/dist/summernote.css',
-						dest: 'public/css/vendor/summernote.css'
-					},
-          {
-						src: 'public/src/components/summernote/dist/summernote.min.js',
-						dest: 'public/js/vendor/summernote.min.js'
-					},
-          {
-						src: 'public/src/components/jquery.serializeJSON/jquery.serializeJSON.min.js',
-						dest: 'public/js/vendor/jquery.serializeJSON.min.js'
-					},
-          {
-            src: 'public/src/components/jvectormap/jquery-jvectormap.js',
-            dest: 'public/js/vendor/jquery-jvectormap.js'
-          },
-          {
-            src: 'public/src/components/jvectormap/jquery-jvectormap.css',
-            dest: 'public/css/vendor/jquery-jvectormap.css'
-          },
-          {
-            src: 'public/src/components/chart.js/dist/Chart.min.js',
-            dest: 'public/js/vendor/Chart.min.js'
-          },
 				]
 			}
 		},
-    */
+
     uglify: {
       js: {
         options: {
@@ -174,7 +94,7 @@ module.exports = function(grunt) {
     'build', [
 		    'copy:build',
 				'less:always',
-				'concat:dist',
+				// 'concat:dist',
 				'uglify:js'
 		]
   );
---
+++ src/js/rda.js
@@ -0,0 +1,12 @@
+var RDA = {
+
+  init: function() {
+
+  },
+}
+
+RDA.init();
+
+$(document).ready(function(){
+     $(".popcontent-text").mCustomScrollbar();
+});
