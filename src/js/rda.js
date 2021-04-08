177eb3a4da4c79d0614f0f5aac408cb32227d062
IronRunes <tech@alanzard.com>
2021-04-08 16:18:20 +0200

    timeline pt 1

--- src/js/rda.js
+++ src/js/rda.js
@@ -1,12 +1,25 @@
 var RDA = {
 
   init: function() {
-
+    this.popContent();
   },
+
+  popContent: function(){
+    $('.s, .d').click(function(e){
+      $('.popcontent').addClass('hide');
+      $(this).next('.popcontent').removeClass('hide');
+    });
+    $('.popcontent .right .close').click(function(e){
+      $(this).parent().parent('.popcontent').addClass('hide');
+    });
+  }
+
 }
 
-RDA.init();
+
 
 $(document).ready(function(){
-     $(".popcontent-text").mCustomScrollbar();
+  RDA.init();
+  $('.popcontent-text').mCustomScrollbar();
+  $('.timeline').mCustomScrollbar();
 });
